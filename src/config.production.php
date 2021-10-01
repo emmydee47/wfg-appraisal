3a8fb8ac5472e648d9044158b6234942bdc5c67b
emmydee47 <emmydee47@gmail.com>
2021-10-01 21:23:51 +0100

    updates to JWT key

--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "i5RbVwUU4yf4QdrA", // API Secret Key
+        "SECRET_KEY" => "u0qLMElVh5pi4h2O", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
